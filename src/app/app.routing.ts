38ec84789c6bf049d8de9c5adae9202bcd312bf6
privalou <artem.privalov@waveaccess.ru>
2019-07-11 14:04:13 +0400

    added initial routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import {RouterModule, Routes} from '@angular/router';
+import {TableComponent} from './core/table.component';
+import {FormComponent} from './core/form.component';
+
+const routes: Routes = [
+  {path: 'form/edit', component: FormComponent},
+  {path: 'form/create', component: FormComponent},
+  {path: '', component: TableComponent}];
+export const routing = RouterModule.forRoot(routes);
