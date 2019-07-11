4c9b7821fa76a090ce7055df703f82b4d7b7055c
privalou <artem.privalov@waveaccess.ru>
2019-07-11 13:46:52 +0400

    preparing for the routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,30 +4,25 @@ import {FormsModule} from '@angular/forms';
 import {ModelModule} from '../model/model.module';
 import {TableComponent} from './table.component';
 import {FormComponent} from './form.component';
-import {MODES, SHARED_STATE, SharedState} from './sharedState.model';
+import {SHARED_STATE, SharedState} from './sharedState.model';
 import {Subject} from 'rxjs';
 import {StatePipe} from './called state.pipe';
-import {Message} from '../messages/message.model';
 import {Model} from '../model/repository.model';
 import {MessageService} from '../messages/message.service';
 import {MessageModule} from '../messages/message.module';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ModelModule, MessageModule],
-  declarations: [TableComponent, FormComponent, StatePipe], exports: [ModelModule, TableComponent, FormComponent], providers: [{
+  declarations: [TableComponent, FormComponent, StatePipe],
+  exports: [ModelModule, TableComponent, FormComponent],
+  providers: [{
     provide: SHARED_STATE,
     deps: [MessageService, Model],
     useFactory: (messageService, model) => {
-      const subject = new Subject<SharedState>();
-      subject.subscribe(m => messageService.reportMessage(
-        new Message(MODES[m.mode] + (m.id !== undefined
-          ? ` ${model.getProduct(m.id).name}` : '')))
-      );
-      return subject;
+      return new Subject<SharedState>();
     }
   }]
 })
 export class CoreModule {
 }
-
 
