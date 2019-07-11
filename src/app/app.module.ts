18b3b2ff3a191e930401e5847f4ceea2c47f3b0c
aprivalov <artem.privalov@waveaccess.ru>
2019-07-12 01:55:21 +0400

    finished routing features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {ModelModule} from './model/model.module';
 import {CoreModule} from './core/core.module';
 import {MessageModule} from './messages/message.module';
 import {AppComponent} from './app.component';
 import {routing} from './app.routing';
 import {TermsGuard} from './terms.guard';
+import {LoadGuard} from './load.guard';
 
 @NgModule({
-  imports: [BrowserModule, ModelModule, CoreModule, MessageModule, routing],
-  declarations: [AppComponent],
-  providers: [TermsGuard],
+  imports: [BrowserModule, CoreModule, MessageModule, routing], declarations: [AppComponent],
+  providers: [TermsGuard, LoadGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/ondemand/second.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pa-second',
+  template: `
+    <div class="bg-info text-white p-2">Second Component</div>`
+})
+export class SecondComponent {
+}
